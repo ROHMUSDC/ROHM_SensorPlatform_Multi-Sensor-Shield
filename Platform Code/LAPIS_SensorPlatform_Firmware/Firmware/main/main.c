9b222d8c914be5dcc7038177009afd530624a6b3
kbahar <krisbahar@gmail.com>
2015-06-16 11:12:34 -0700

    Added in GPLv3 into Source Files (main.c, *.ino)
    Also added first draft of Arduino User's Manual... this is a work in
    progress but is starting

--- Platform Code/LAPIS_SensorPlatform_Firmware/Firmware/main/main.c
+++ Platform Code/LAPIS_SensorPlatform_Firmware/Firmware/main/main.c
@@ -5,6 +5,22 @@
 // Started:  July 8th, 2014
 // Purpose:	 Firmware for Q112 for Sensor Platform Board 
 // Updated:	 July 8th, 2014
+/*
+	Copyright (C) 2015 ROHM USDC Applications Engineering Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 //*****************************************************************************
 //#define DebugSensor	16
 
